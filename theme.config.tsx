87973556c7933aa807cc62a15d69415157679b89
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-09-17 23:34:04 +0530

    added two algo folder

---
+++ theme.config.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Algo Friend</span>,
+  project: {
+    link: 'https://github.com/ninadvyas',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/ninadvyas',
+ 
+}
+
+export default config
